f3224e22a8b20e7987510784442d9551242200a4
Augusto Correia <augustocorreia@frwk.com.br>
2022-01-03 22:43:12 -0300

    Novo commit

---
+++ src/main/java/br/com/profissaoprogramador/appmicrogestor/AppMicroGestorApplication.java
@@ -0,0 +1,13 @@
+package br.com.profissaoprogramador.appmicrogestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMicroGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMicroGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/profissaoprogramador/appmicrogestor/AppMicroGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.profissaoprogramador.appmicrogestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMicroGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
