fd10094b6c7d6b3076c2bc55cf97556faabed240
Karpeg Nogueira <karpegsilva92@gmail.com>
2022-01-24 01:06:51 -0300

    Configuration WebSecurityConfig

--- src/main/java/br/com/profissaoprogramador/appmicrogestor/security/PasswordEncoder.java
+++ src/main/java/br/com/profissaoprogramador/appmicrogestor/security/PasswordEncoder.java
@@ -1,5 +1,17 @@
 package br.com.profissaoprogramador.appmicrogestor.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
 }
--- src/main/java/br/com/profissaoprogramador/appmicrogestor/security/config/WebSecurityConfig.java
+++ src/main/java/br/com/profissaoprogramador/appmicrogestor/security/config/WebSecurityConfig.java
@@ -1,10 +1,15 @@
 package br.com.profissaoprogramador.appmicrogestor.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.com.profissaoprogramador.appmicrogestor.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 
 @Configuration
@@ -12,6 +17,10 @@ import lombok.AllArgsConstructor;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
+	private final AppUserService appUserService = null;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder = null;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http
@@ -23,6 +32,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.authenticated().and()
 			.formLogin();
 	}
+
 	
-	//MINUTO 36:24
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(daoAuthenticationProvider());
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(bCryptPasswordEncoder);
+		provider.setUserDetailsService(appUserService);
+		return provider;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
